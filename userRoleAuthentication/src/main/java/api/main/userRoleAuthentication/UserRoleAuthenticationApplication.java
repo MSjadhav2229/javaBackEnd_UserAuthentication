5eb6d99c9c93ffc5bfb75a0707e79f8ce2f50d24
Milindjadhav1507 <milindjadhav@MyrahConsultingServicesPriv.onmicrosoft.com>
2025-04-19 14:15:52 +0530

    CODE PUSH HERE..

---
+++ userRoleAuthentication/src/main/java/api/main/userRoleAuthentication/UserRoleAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package api.main.userRoleAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRoleAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRoleAuthenticationApplication.class, args);
+	}
+
+}
---
+++ userRoleAuthentication/src/test/java/api/main/userRoleAuthentication/UserRoleAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package api.main.userRoleAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRoleAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
